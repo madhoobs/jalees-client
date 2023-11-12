74864280756c61c108be02d0b377fe69588974fd
Hasan Almadhoob <hasan@madhoob.com>
2023-11-12 03:35:39 +0300

    Add Login functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,118 @@
-const Login = () => {
-  return <div></div>
+import * as React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { LoginUser } from '../services/Auth'
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Grid,
+  Box,
+  Typography,
+  Container
+} from '@mui/material'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme()
+
+const Login = ({ setUser }) => {
+  let navigate = useNavigate()
+
+  const [formValues, setFormValues] = useState({ username: '', password: '' })
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const payload = await LoginUser(formValues)
+    setFormValues({ username: '', password: '' })
+    setUser(payload)
+    navigate('/')
+  }
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="username"
+              autoComplete="email"
+              autoFocus
+              onChange={handleChange}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="plainPassword"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={handleChange}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/register" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  )
 }
 
 export default Login
