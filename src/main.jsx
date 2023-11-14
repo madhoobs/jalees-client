84ea57120d2af2ae071d89a794896352ce65a749
Hasan Almadhoob <hasan@madhoob.com>
2023-11-14 23:46:13 +0300

    Change theme colors

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FF5A60'
+    },
+    secondary: {
+      main: '#E31C5F' //orange-ish color
+    }
+  }
+})
 createRoot(document.getElementById('root')).render(
-  <Router>
-    <App />
-  </Router>
+  <ThemeProvider theme={theme}>
+    <Router>
+      <App />
+    </Router>
+  </ThemeProvider>
 )
